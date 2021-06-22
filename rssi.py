816c703e830d06fefe3c90612549493cd95ae994
rohansreenivasan5 <85949166+rohansreenivasan5@users.noreply.github.com>
2021-06-22 15:43:21 -0400

    Update rssi.py

--- rssi.py
+++ rssi.py
@@ -14,3 +14,38 @@ ssids = ['dd-wrt','linksys']
 ap_info = rssi_scanner.getAPinfo(networks=ssids, sudo=True)
 
 print(ap_info)
+
+from rssi import RSSI_Localizer
+rssi_localizer_instance = RSSI_Localizer()
+
+accessPoint = {
+     'signalAttenuation': 3, 
+     'location': {
+         'y': 1, 
+         'x': 1
+     }, 
+     'reference': {
+         'distance': 4, 
+         'signal': -50
+     }, 
+     'name': 'dd-wrt'
+}
+signalStrength = -69
+
+distance = rssi_localizer_instance.getDistanceFromAP(accessPoint, signalStrength)
+print(distance)
+
+# prints
+# {
+#     'signalAttenuation': 3, 
+#     'location': {
+#         'y': 1, 
+#         'x': 1
+#     }, 
+#     'reference': {
+#         'distance': 4, 
+#         'signal': -50
+#     }, 
+#     'name': 'dd-wrt',
+#     'distance': 2
+# }
